4a13b5b7d91c36c2dc8ae17008950281d40014fb
JeffCost <jefersonc@gmail.com>
2013-06-16 15:14:43 -0500

    Updates thumbnails module routes. Now we can just create the documentation without have to modify the index.html file "src" attributes to use "docs/"

--- routes/backend/docs.php
+++ routes/backend/docs.php
@@ -3,9 +3,11 @@
 Route::any(ADM_URI.'/(:bundle)/docs/(:all?)', function($route = 'index.html') {
     
     \Config::set('application.profiler', false);
+
+    $docsPath = Bundle::path('thumbnails').'docs/'.$route;
     
-    $mime     = File::extension(Bundle::path('thumbnails').'docs/' . $route);
-    $response = File::get(Bundle::path('thumbnails').'docs/' . $route);
+    $mime     = File::extension($docsPath);
+    $response = File::get($docsPath);
     $headers  = array('Content-type' => Config::get("mimes.{$mime}"));
     
     return Response::make($response, 200, $headers);   
