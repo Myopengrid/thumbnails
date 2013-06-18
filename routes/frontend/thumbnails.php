58b6665ca2cdb1117a6b24179b967ec15183d5ac
JeffCost <jefersonc@gmail.com>
2013-06-17 21:44:16 -0500

    Adds routing for thumbnails. We dont need a controller to only return an image.

--- routes/frontend/thumbnails.php
+++ routes/frontend/thumbnails.php
@@ -1,12 +1,18 @@
 <?php
 
-Route::any('(:bundle)/(:all?)', function($route = '') {
-    
-    $route = explode('/', $route);
-    
-    $method = isset($route[0]) ? $route[0] : 'thumb';
-    
-    array_shift($route);
+Route::get('(:bundle)/thumb/(:any)/(:any)/(:all?)', function ($size, $mode, $path) {
+        
+    $thumb      = new Thumbnails\Thumb;
+    $response   = $thumb->thumbnail($path, array('size' => $size, 'mode' => $mode, 'resource' => true));
+    $finfo      = new finfo(FILEINFO_MIME_TYPE);
 
-    return Controller::call("thumbnails::frontend.thumbnails@{$method}", $route);
+    
+    $headers = array(
+        'Accept-Ranges' => 'bytes',
+        'Cache-Control' => 'must-revalidate max-age=2592000',
+        'Content-type'  => $finfo->buffer($response),
+        'Date'          => gmdate('D, d M Y H:i:s \G\M\T', time()),
+        'Expires'       => gmdate('D, d M Y H:i:s \G\M\T', time() + 33600),
+    );
+    return Response::make($response, 200, $headers);
 });
